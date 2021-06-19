e51d4f72d9fa058a2863a4df03d5820458331642
marlonaesparza <marlon@DESKTOP-6P13I3V.localdomain>
2021-06-18 17:43:29 -0700

    Created Product Context: -Using a react context to save a list of test data -Set up a ProductContextProvider that wraps the app

--- client/index.js
+++ client/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+
 import App from './app/App';
+import ProductsContextProvider from './app/contexts/ProductsContext.jsx';
+
 import './utils/bulma/styles.scss'
 
 
@@ -9,7 +12,9 @@ const root = document.getElementById('root');
 
 ReactDOM.render(
   <BrowserRouter>
-    <App/>
+    <ProductsContextProvider>
+      <App/>
+    </ProductsContextProvider>
   </BrowserRouter>,
   root
 );
